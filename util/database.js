143be24c8334637b79c03016679abadc29b7a483
turosgolyo <papp.dani2206@gmail.com>
2025-05-23 13:22:08 +0200

    jav

--- util/database.js
+++ util/database.js
@@ -27,7 +27,7 @@ const books = [
     { id: 1, title: 'Book 1', author: 'Author 1' },
     { id: 2, title: 'Book 2', author: 'Author 2' },
     { id: 3, title: 'Book 3', author: 'Author 3' },
-    { id: 4, title: 'Book 4', author: 'Author 4' },
+    { id: 4, title: 'Book 4', author: 'Author 4' }
 ]
 
 //for (const book of books) {
